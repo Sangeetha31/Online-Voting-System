ff843f54cd79496f8548357bf03090f03300b9b2
Janhavi23 <janhavi1dkulkarni@gmail.com>
2021-06-16 11:40:11 +0530

    Updated with master branch...

--- app/src/main/java/com/example/onlinevoting/MainActivity.java
+++ app/src/main/java/com/example/onlinevoting/MainActivity.java
@@ -9,16 +9,30 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     Button register;
     Button login;
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register=(Button) findViewById(R.id.register);
-        login=(Button) findViewById(R.id.loginbtn);
-        register.setOnClickListener(new View.OnClickListener() {
+        mLogOutBtn = findViewById(R.id.log_out_btn);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this , login.class));
+                finish();
+            }
+        });
+      /*  register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, register.class);
@@ -31,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent1 = new Intent(MainActivity.this, login.class);
                 startActivity(intent1);
             }
-        });
+        });*/
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            startActivity(new Intent(MainActivity.this , login.class));
+            finish();
+        }
     }
 }
